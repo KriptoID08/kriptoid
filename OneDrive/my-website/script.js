aaed7466d6b555e82d3e5f2cdece948ba1cfdb55
KriptoID <kriptoid08@gmail.com>
2025-04-06 16:01:42 +0700

    Menambahkan website kelas dengan foto dan link YouTube

--- OneDrive/my-website/script.js
+++ OneDrive/my-website/script.js
@@ -1,17 +1,6 @@
-const coverImages = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    // Tambahkan gambar lain di sini
-  ];
-  
-  const coverContainer = document.querySelector('.cover-container');
-  
-  // Menampilkan gambar-gambar cover secara dinamis
-  coverImages.forEach(image => {
-    const img = document.createElement('img');
-    img.src = `images/${image}`;
-    img.alt = `Cover ${image}`;
-    coverContainer.appendChild(img);
-  });
-  
+// Contoh interaktivitas jika diperlukan
+document.querySelectorAll('.module a').forEach(link => {
+    link.addEventListener('click', () => {
+        alert("Anda akan diarahkan ke YouTube");
+    });
+});
